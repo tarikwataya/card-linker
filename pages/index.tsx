244d1acdb4e039da12f6b2f2479721c7c0ae116c
Tárik Wataya <tarikwataya@gmail.com>
2021-03-14 23:24:13 -0300

    ✔ Novos cards e animações

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,11 +9,11 @@ interface ICardProps {
 
 const Card: React.FC<ICardProps> = ({ href, imgSrc, tittle }) => (
   <Link href={href}>
-    <div className="h-20 mt-6 flex flex-row bg-gray-400 bg-opacity-20 rounded-xl cursor-pointer">      
+    <div className="transition duration-500 ease-in-out h-14 mt-6 flex flex-row bg-gray-400 bg-opacity-20 rounded-xl cursor-pointer hover:bg-gray-500 hover:bg-opacity-20 transform hover:-translate-y-1 hover:scale-110">      
       <img src={imgSrc} className="h-full p-4" />
-      <div className="mt-4">
-        <p className="text-white text-base">{tittle}</p>
-        <p className="text-gray-300 text-sm">test description</p>
+      <div className="mt-3 sm:mt-2">
+        <p className="text-white text-lg sm:text-base">{tittle}</p>
+        <p className="hidden sm:block text-gray-300 text-sm">test description</p>
       </div>
     </div>
   </Link>
@@ -23,19 +23,21 @@ export default function Home() {
   return (
     <div className="flex justify-center">
 
-      <div className="rounded-3xl overflow-hidden shadow w-4/5 sm:w-4/5 md:w-2/5 my-3 bg-gray-400 bg-opacity-20">
-        <img src="https://i.imgur.com/GUIuOIN.jpg" className="w-full h-40 border-b-4 border-white" />
+      <div className="rounded-3xl overflow-hidden shadow w-4/5 sm:w-4/5 md:w-2/5 my-3 bg-gray-500 bg-opacity-20">
+        <img src="https://i.imgur.com/GUIuOIN.jpg" className="w-full h-36 border-b-4 border-white" />
         <div className="flex justify-center -mt-8">
-            <img src="https://github.com/tarikwataya.png" className="w-28 h-28 rounded-full border-solid border-4 -mt-6 border-white" />		
+            <img src="https://github.com/tarikwataya.png" className="transition duration-500 ease-in-out w-28 h-28 rounded-full border-solid border-4 -mt-6 border-white transform hover:-translate-y-1 hover:scale-125" />		
         </div>
         <div className="text-center px-3 pt-2">
-          <h3 className="text-white text-lg bold font-sans">Tárik Wataya</h3>
+          <h3 className="text-white text-xl bold font-sans">Tárik Wataya</h3>
           <p className="font-sans font-light text-gray-300">Front-End Developer</p>
         </div>
         <div className="pl-10 pr-10 pb-10 flex flex-col">
-          <Card href="/tailwind" imgSrc="/img/instagram.svg" tittle="Access my instagram" />
-          <Card href="/nextjs" imgSrc="/img/github.svg" tittle="The best Github"/>
-          <Card href="/nextjs" imgSrc="/img/github.svg" tittle="The best Github"/>
+          <Card href="/tailwind" imgSrc="/img/instagram.svg" tittle="My instagram" />
+          <Card href="/nextjs" imgSrc="/img/github.svg" tittle="My Github"/>
+          <Card href="/nextjs" imgSrc="/img/portfolio.svg" tittle="My Portfolio"/>
+          <Card href="/nextjs" imgSrc="/img/github.svg" tittle="My Github"/>
+          <Card href="/nextjs" imgSrc="/img/portfolio.svg" tittle="My Portfolio"/>
         </div>
 
       </div>
